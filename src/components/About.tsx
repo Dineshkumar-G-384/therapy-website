3af38595db2216296987e40d46fa451d9657223b
Dineshkumar-G-384 <dinshkumarg@gmail.com>
2025-07-02 09:37:16 +0530

    Refine About section spacing and center content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <section id="about" className="scroll-mt-40 pt-40 pb-36 bg-white px-4">
+    <section id="about" className="scroll-mt-48 pt-48 pb-40 bg-white px-4">
       <div className="max-w-6xl mx-auto flex flex-col-reverse md:flex-row items-center gap-10">
         
         {/* Left*/}
